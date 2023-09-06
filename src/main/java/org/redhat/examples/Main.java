cbb5d4d5006cffa652828bb35a4a3a04feb629cc
Zvi Grinberg <zgrinber@redhat.com>
2023-09-06 15:12:48 +0300

    enhance comment
    Signed-off-by: Zvi Grinberg <zgrinber@redhat.com>

--- src/main/java/org/redhat/examples/Main.java
+++ src/main/java/org/redhat/examples/Main.java
@@ -12,6 +12,13 @@ public class Main {
 //        Logger log = LoggerFactory.getLogger("test-java-api");
 
         //uncomment these 2 lines to activate SL4J Jul Bridge, if used Library application using Java Logging(JUL) ==> System.getLogger
+        // If client library using slf4j facade, like this example code, then keep it commented and the client caller ( this example) will choose
+        // the implementation/binding/provider for logging framework(logback/log4j , etc) - the idea is that logs will be formatted and handled by the same
+        // logging framework, and sl4fj gives you an option to let it happen in both cases:
+        // 1. slf4j is used by both client and library - in this case the following 2 lines are redundant.
+        // 2. slf4j is used only on the client and no in the library - in this case you need to use slf4j bridge for the logging framework being used by the library, in this case
+        //    it's a bridge to intercept Java JUL LogRecord(s) by SLF4J Bridge handler (in the library), and route it to the SLF4J api (as you called slf4j logger.log directly, like here in the client).
+        // more details at https://www.slf4j.org/legacy.html
 //        SLF4JBridgeHandler.removeHandlersForRootLogger();
 //        SLF4JBridgeHandler.install();
         Logger logger = LoggerFactory.getLogger(Main.class.getClass().getName());
